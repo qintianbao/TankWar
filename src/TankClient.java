b4e2f54e3f98fb61dedeae8b11ca283ce29feadb
qinye <763145819@qq.com>
2019-11-13 00:52:57 +0800

    test commit message

---
+++ src/TankClient.java
@@ -0,0 +1,25 @@
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+
+public class TankClient extends Frame{
+	public void lauchFrame() {
+		this.setLocation(180,90);
+		this.setSize(800,600);
+		this.setTitle("TankWar");
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		this.setResizable(false);
+		setVisible(true);
+	}
+	
+	
+	public static void main(String[] args) {
+		TankClient client = new TankClient();
+		client.lauchFrame();
+	}
+}
